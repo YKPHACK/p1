edb6d2cf5b745a4cd9941356e5caf11f12e579cf
YKPHACK <89974989+YKPHACK@users.noreply.github.com>
2021-10-03 19:34:03 +0530

    Update A very big sum.java

--- A very big sum.java
+++ A very big sum.java
@@ -1,21 +1,25 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
 
-public class ykp {
-
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int t = s.nextInt();
-        long sum = 0;
-        while(t-- > 0){
-            sum += s.nextInt();
-        }
-        System.out.println(sum);
-    }
-
-}
+public class ykp {  
+    public static void main(String[] args) { 
+        
+        int n;  
+        Scanner sc=new Scanner(System.in);    
+        //reading the number of elements from the that we want to enter  
+        n=sc.nextInt();  
+        //creates an array in the memory of length 10  
+        long[] array = new long[n];  
+        for(int i=0; i<n; i++)  
+        {  
+        //reading array elements from the user   
+        array[i]=sc.nextInt();  
+        }   
+        //Initialize array   
+        long sum = 0;  
+        //Loop through the array to calculate sum of elements  
+        for (int i = 0; i < n; i++) {  
+           sum = sum + array[i];  
+        }  
+        System.out.println(sum);  
+    }  
+}  
